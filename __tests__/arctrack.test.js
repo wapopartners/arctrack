4a26e4cbdf14cf79fc06d96212d2872f0039c9b5
danasselin <dasselin86@gmail.com>
2018-01-03 10:18:47 -0500

    Start rewriting tests using jest mocks

--- __tests__/arctrack.test.js
+++ __tests__/arctrack.test.js
@@ -1,27 +1,41 @@
 const Arctrack = require('../index');
 
-let at1;
-let name = 'test';
-const opts = {
-  initCb: function() {
-    constructorTest = 1;
-  }
-}
-let constructorTest = 0;
-
-window.test = { property1: 'foo' };
-
 describe('arctrack', () => {
-    at1 = new Arctrack({ name, opts });
-    beforeEach(() => {
-        window.dispatchEvent(new Event('load'));
-    })
-    describe('#constructor', () => {
-        it('receives analytics data when the page has loaded', () => {
-            expect(at1.pageData).toBe(window.test);
-        });
-        it('executes a \'load\' event handler when the page has loaded', () => {
-            expect(constructorTest).toBe(1);
-        });
+  const opts = {
+    initCb: jest.fn(),
+    clickCb: jest.fn(),
+  }
+  window.test = { property1: 'foo' };
+  let at1 = new Arctrack({ name: 'test', opts });
+
+  beforeAll(() => {
+    window.dispatchEvent(new Event('load'));
+  });
+
+  describe('#constructor', () => {
+    it('receives analytics data when the page has loaded', () => {
+      const { pageData } = at1;
+      expect(pageData).not.toBeUndefined();
+      expect(pageData.property1).toBe('foo');
     });
+
+    it('executes an init callback when the page has loaded', () => {
+      const { calls } = opts.initCb.mock;  
+      expect(calls.length).toBe(1);
+    });
+  });
+
+  describe('#trackClick', () => {
+    beforeAll(() => {
+      document.body.innerHTML = '<a data-test="someClick"></a>';
+    })
+    it('delegates a click handler to fire on click event', () => {
+      const [link] = document.querySelectorAll('[data-test]');
+      // const event = new Event('click');
+      // link.dispatchEvent(event);
+      // const { calls } = opts.clickCb.mock;
+      // console.log(calls);
+      // expect(calls.length).toBe(1);
+    });
+  });
 });
