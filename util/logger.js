8538d93110e95519b6ed99b72488e778c6bb2765
danasselin <dasselin86@gmail.com>
2018-01-08 14:00:16 -0500

    Update logger

--- util/logger.js
+++ util/logger.js
@@ -2,8 +2,9 @@ class Logger {
   defaultError(msg) {
     console.error(`Arctrack constructor failed with the following error: `, msg);
   }
-  callbackFailed(method, msg) {
-    console.error(`Arctrack#${method} failed while trying to execute your callback: `, msg);
+  methodFailed(method, msg, callback) {
+    const callbackMsg = callback ? `while trying to execute your callback: ${callback}` : '';
+    console.error(`Arctrack#${method} failed ${callbackMsg}, with the following error`, msg);
   }
 }
 
