56575618660fee71a6dc9553032b3504bdd76e9d
danasselin <dasselin86@gmail.com>
2018-01-30 09:26:07 -0500

    Add trackJs functionality

--- jestPreprocess.js
+++ jestPreprocess.js
@@ -1,3 +1,6 @@
-const babelOptions = { presets: ['env'] };
+const babelOptions = {
+  presets: ['env', 'stage-2'],
+  plugins: ['transform-decorators-legacy'],
+};
 
 module.exports = require('babel-jest').createTransformer(babelOptions);
